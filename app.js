c16d7cc8a52d2c9e6cda3fa32b015f6571245c88
joe <joelibramonte123@gmail.com>
2019-09-04 15:57:25 +0800

    1

--- app.js
+++ app.js
@@ -292,15 +292,14 @@ function handleAddress(sender_psid, received_message){
     });
 
    });
-
-
+  };
+  
 // ------- SAVE ACTION TO DATABASE ------- //
   user.saveUser(sender_psid, payload, result => {
     if (result.success) {
       console.log(`Messenger ID ${sender_psid} action saved to the database.`);
         }
       });
-    };
 
 
 }
